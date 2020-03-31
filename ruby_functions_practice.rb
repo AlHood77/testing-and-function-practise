80f02308d1630790e64b8ac8c83cbd053b9d1a9c
Alan <alanhood77@gmail.com>
2020-03-31 17:45:19 +0100

    initial commit of testing practise files

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,97 @@
+# def return_10
+#     return 10
+# end
+
+# def add (first_number, second_number)
+#     return (first_number + second_number)
+# end
+
+# p add(1, 2)
+
+# def subtract (first_number, second_number)
+#     return (first_number - second_number)
+# end
+
+# p subtract(1, 2)
+
+
+# def multiply (first_number, second_number)
+#     return (first_number * second_number)
+# end
+
+# p multiply(1, 2)
+
+# def divide (first_number, second_number)
+#     return (first_number / second_number)
+# end
+
+# p divide(10, 2)
+
+# def length_of_string(string)
+#     return (string.length)
+# end
+ 
+# p length_of_string("A string of length 21")
+
+# def join_string(string_1, string_2)
+#     return (string_1 + string_2)
+# end
+
+# p join_string "Mary had a little lamb", "its fleece was white as snow"
+
+# def add_string_as_number (string_1, string_2).to_i
+#     sum = (string_1 + string_2
+#     return sum
+# end
+
+# p sum ("1", "2")
+
+def number_to_full_name__month_1(month1)
+    return 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
